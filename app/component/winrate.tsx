95ab5fe26d493a0b3ea3cbb9be3020bed3f0a8a5
qyzh <mrveranda@gmail.com>
2025-03-10 18:10:22 +0700

    Remove unused CardFooter component from winrate

--- app/component/winrate.tsx
+++ app/component/winrate.tsx
@@ -7,7 +7,6 @@ import {
   Card,
   CardContent,
   CardDescription,
-  CardFooter,
   CardHeader,
   CardTitle,
 } from "../component/ui/Card"
@@ -123,14 +122,6 @@ const winlose = `https://api.opendota.com/api/players/${userID}`; // 'https://ap
           </RadialBarChart>
         </ChartContainer>
       </CardContent>
-      <CardFooter className="flex-col gap-2 text-sm">
-        <div className="flex items-center gap-2 font-medium leading-none">
-          Trending up by 5.2% this month <TrendingUp className="h-4 w-4" />
-        </div>
-        <div className="leading-none text-muted-foreground">
-          Showing total visitors for the last 6 months
-        </div>
-      </CardFooter>
     </Card>
    </div>
       );
